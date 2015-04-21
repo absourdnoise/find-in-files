0074bdebe31de7278e24654eea1572b84feaa22a
Absurd Noise <absourd.noise@gmail.com>
2015-04-21 18:11:18 +0300

    cleaned code

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"runtime"
-//	"log"
 	"os"
-//	"regexp"
 	"strings"
 	"path/filepath"
 )
@@ -23,18 +21,13 @@ func main()	{
 	fileTypes = []string{".html", ".asp"}
 	var tasks []string
 
-//	filepath.Walk(rootDir, scanFs);
 	MAXPROC = runtime.NumCPU() - 1
 
 	dirf, _ := ioutil.ReadDir(rootDir)
 
-//	dirft := dirf[4:8]
-//	println(len(dirf), MAXPROC, len(dirf) / MAXPROC);
 
 	for i, f := range dirf {
-//		fmt.Println(i, f.Name(), (i) % MAXPROC)
 		if f.IsDir() {
-//			tasks[j][] = f.Name();
 			tasks = append(tasks, f.Name())
 			if i > 0 && (i % MAXPROC) == 0 {
 				go doSearchInSlice(tasks);
@@ -76,40 +69,20 @@ func searchInFile(findTo string, file string) (results map[int]string, err error
 	return results, err
 }
 
-
-
 func scanFs(path string, f os.FileInfo, err error) error {
-
 	for _, ft := range fileTypes{
 		idx := len(path) - len(ft)
 
-//		fmt.Println(path, ft, path[idx:])
 		if idx >= 0 && path[idx:] == ft {
-//			fmt.Println(path)
-//			finded, err := searchInFile(findTo, path);
-//			go searchInFile(findTo, path);
 			searchInFile(findTo, path);
-	/*		if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-*/
-/*			if len(finded) > 0 {
-				for num, str := range finded {
-					fmt.Printf("%d: %s", num, str)
-				}
-			}*/
 		}
 	}
-
 	return nil
 }
 
 func doSearchInSlice(dirs []string) error {
 	for _, f := range dirs {
-//		fmt.Println(i, f);
 		filepath.Walk(rootDir + f + "/templates", scanFs);
 	}
-//	fmt.Printf("\n\n")
 	return nil
 }
